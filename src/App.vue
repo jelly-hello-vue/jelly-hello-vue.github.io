ac2815a5a44e3872f814c8a8347ca4787f0ed976
jaelinny <leenlyn3127@gmail.com>
2024-01-31 12:09:13 +0900

    var

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 
 import { ref } from 'vue'
 
-const message = ref('Hello World!')
+const message = ref({name: "LIN", age: 30})
+const name = "LIN"
 
 </script>
-
 <template>
 <h1>{{ message }}</h1>
 </template>
