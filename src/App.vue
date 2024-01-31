e0a6737b64f2416aa289e8393f8622c1bb00e109
jaelinny <leenlyn3127@gmail.com>
2024-01-31 15:23:10 +0900

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const parentMessage = ref('Category')
-const items = ref([{ name: '푸드', code: 'CATE_01' }, { name: '음료', code: 'CATE_02' }, { name: '상품', code: 'CATE_03' }])
-const categories = ref([
-  { "name": "푸드", "image_url": "https://image-server.kr/def/cate-1.jpg", "code": "CATE_01" },
-  { "name": "음료", "image_url": "https://image-server.kr/def/cate-2.jpg", "code": "CATE_02" },
-  { "name": "상품", "image_url": "https://image-server.kr/def/cate-3.jpg", "code": "CATE_03" },
-  { "name": "고객센터", "image_url": "https://image-server.kr/def/cate-4.jpg", "code": "CATE_04" }
-])
+const categories = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await fetch('/src/assets/category.json')
+    const data = await response.json()
+    categories.value = data
+  } catch (error) {
+    console.error('Error fetching category data:', error)
+  }
+})
 </script>
 
 <template>
   <div>
-
     <ul>
       <li v-for="(category, index) in categories" :key="index">
-        {{ parentMessage }} - {{ index + 1 }} - Name: {{ category.name }} - Code: {{ category.code }}
+        {{ parentMessage }} - Name: {{ category.name }} - Code: {{ category.code }}
       </li>
     </ul>
   </div>
