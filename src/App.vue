eeeaee687b75da726a95ccb1311a9a8eea576409
jaelinny <leenlyn3127@gmail.com>
2024-01-31 11:48:29 +0900

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <script setup>
-import { ref, onMounted } from 'vue'
 
-//reactive state
-const count = ref({
-      cnt: 0,
-      name: "PARK",
- })
+import { ref } from 'vue'
 
-// functions that mutate state and trigger updates
-function increment() {
-    count.value.cnt = count.value.cnt + 1
-    count.value.name = count.value.name.split('').reverse().join('')
- }
+const message = ref('Hello World!')
 
-//lifecycle hooks
-onMounted(() => console.log('The initial count is ${count.value}.'))
- </script>
+</script>
 
-    <template>
-    <button @click="increment">Count is: {{ count.cnt }} {{ count.name }}</button>
-    </template>
-
-    <style scoped>
-    button {
-	font-weight: bold;
-    }
-</style>
+<template>
+<h1>{{ message }}</h1>
+</template>
