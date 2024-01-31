36f14e1c417c63be5186503d3335306e9c50fce9
jaelinny <leenlyn3127@gmail.com>
2024-01-31 10:43:47 +0900

    update App.vue2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
 const count = ref(0)
-    </script>
+
+function increment()
+    count = count + 1
+ }
+
+onMounted(() => {
+	console.log('The initial count is ${count.value}.')
+})
+ </script>
 
     <template>
-    <button @click="count++">Count is: {{ count }}</button>
+    <button @click="increment">Count is: {{ count }}</button>
     </template>
 
     <style scoped>
