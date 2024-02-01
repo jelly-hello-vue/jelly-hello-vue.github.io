688f37e8ba09d621faa77ae4b62f362c4b5fb4bb
jaelinny <leenlyn3127@gmail.com>
2024-02-01 10:46:05 +0900

    Lifecycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-
-
-const message = ref(' ')
+import { ref, onUpdated } from 'vue'
+const count = ref(0)
+    onUpdated(() => {
+	    console.log(document.getElementById('count').textContent)
+	    })
 </script>
 
 <template>
-<span>Multiline message is:</span>
-<p style="white-space: pre-line;">{{ message }}</p>
-<textarea v-model="message" placeholder="add multiple lines"></textarea>
+<button id="count" @click="count++">{{ count }}</button>
 </template>
