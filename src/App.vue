88ce460661abbbd4c287a66e601409483713e6ad
jaelinny <leenlyn3127@gmail.com>
2024-02-01 10:30:54 +0900

    say Hello

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <script setup>
-import { reactive, computed, ref } from 'vue'
-
-const author = ref({
-name: 'John Doe',
-books: [1, 2, 3]
-})
-
-const check = computed(() => {
-	return author.value.books.lenth > 2 ? 'Yes' : 'No'
-	})
-
-//a computed ref
-//const publishedBooksMessage = computed(() => { return author.value.name.length > 2 ? 'Yes' : 'No' })
+function say(message) {
+    alert(message)
+}
 </script>
 
 <template>
-<span>🐰: </span>
-<span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span>
-<div>computed ⬇️ </div>
-<div>{{ check }}</div>
-
-<!-- <p>Has published books:</p>
-<span>{{ publishedBookMessage }}</span> -->
+<button @click="say('hello')">Say hello</button>
+<button @click="say('bye')">Say bye</button>
 </template>
+
