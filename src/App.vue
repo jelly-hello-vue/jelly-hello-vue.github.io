a6ac75e207e105bfbca5621175564c0aaac29c19
jaelinny <leenlyn3127@gmail.com>
2024-02-01 10:19:46 +0900

    🐰

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
- <script setup>
- import { reactive, computed, ref } from 'vue'
+<script setup>
+import { reactive, computed, ref } from 'vue'
 
- const author = ref({
- name: 'John Doe',
- books: [1, 2, 3]
- })
- </script>
+const author = ref({
+name: 'John Doe',
+books: [1, 2, 3]
+})
 
- <template>
- <p>Has published books:</p>
- <span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span>
- </template>
+const check = computed(() => {
+	return author.value.books.lenth > 2 ? 'Yes' : 'No'
+	})
+
+//a computed ref
+//const publishedBooksMessage = computed(() => { return author.value.name.length > 2 ? 'Yes' : 'No' })
+</script>
+
+<template>
+<span>🐰: </span>
+<span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span>
+<div>computed ⬇️ </div>
+<div>{{ check }}</div>
+
+<!-- <p>Has published books:</p>
+<span>{{ publishedBookMessage }}</span> -->
+</template>
